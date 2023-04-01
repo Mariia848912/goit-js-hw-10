6bce4559a12cf357b2dda2511d9f3a085cb33348
Mariia Sarbieieva <sarbeeva.m.a@gmail.com>
2023-04-01 16:29:54 +0300

    удаляет лишнюю разметку

--- src/js/components/api-service.js
+++ src/js/components/api-service.js
@@ -1,11 +1,8 @@
 const BASE_URL = 'https://restcountries.com/v3.1/name/';
 const options = '?fields=name,capital,population,flags,languages';
-// const country = ukrayina;
-// const fields = '${name.official},capital,population,flags.svg,languages';
+
 export function fetchCountries(nameSearch) {
   const url = `${BASE_URL}${nameSearch}${options}`;
-
-  //   const url = `${BASE_URL}${nameSearch}`;
 
   return fetch(url)
     .then(response => {
